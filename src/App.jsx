1f01a0090122a1e0958334a8a8bf4744bf76f6d9
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-09-01 00:22:16 +0300

    ап
    е

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import Profile from "./components/Profile/Profile";  
-import userData from "./assets/userData.json";  
-// import FriendList from "./components/FriendList/FriendList";  
-import friends from "./assets/friends.json";  
-// import TransactionHistory from "./components/TransactionHistory/TransactionHistory";  
-// import transactions from "./assets/transactions.json";  
+import FriendList from "./components/FriendList/FriendList";
+import Profile from "./components/Profile/Profile";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import userData from "./UserData/userData.json";
+import friends from "./UserData/friends.json";
+import "./App.css";
 
-const App = () => {  
-  return (  
-    <>  
-      <Profile  
-        name={userData.username}  
-        tag={userData.tag}  
-        location={userData.location}  
-        image={userData.avatar}  
-        stats={userData.stats}  
-      />  
-      <FriendList friends={friends} />  
-      <TransactionHistory items={transactions} />  
-    </>  
-  );  
-};  
+function App() {
+  return (
+    <div>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory />
+    </div>
+  );
+}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import "modern-normalize";
 
-createRoot(document.getElementById("root")).render(
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
