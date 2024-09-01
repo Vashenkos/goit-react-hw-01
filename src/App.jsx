da475b3431760d077245b7cb6cb5b7b8e671af81
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-09-02 00:59:49 +0300

    укп

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import FriendList from "./components/FriendList/FriendList";
 import Profile from "./components/Profile/Profile";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-import transactions from "./assets/transactions.json
-"
+import transactions from "./assets/transactions.json"; 
+import transactions from "./assets/transactions.json"; 
 import userData from "./assets/userData.json";
 import friends from "./assets/friends.json";
 import "./App.css"
